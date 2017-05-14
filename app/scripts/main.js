1898f55cef6fd5bff4b1d62fc5665012064bfcf8
cviale <cviale@pelhammedia.com>
2017-05-14 13:27:01 +0200

    Responsive Tablet & mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+	// Hide placeholder
+	$(function () {
+		$('input,textarea').focus(function () {
+			$(this).data('placeholder', $(this).attr('placeholder'))
+				.attr('placeholder', '');
+		}).blur(function () {
+			$(this).attr('placeholder', $(this).data('placeholder'));
+		});
+	});
+});
