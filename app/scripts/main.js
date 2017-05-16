4030f4ac767b5aa3fae92694b4aaa13e7fb7b7f1
cviale <cviale@pelhammedia.com>
2017-05-16 09:22:00 +0200

    Ajustements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,4 +12,40 @@ $(document).ready(function() {
 	$('body').on('touchmove', function (e) {
 		e.preventDefault();
 	});
+
+	function validateEmail(sEmail) {
+		var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+		if (filter.test(sEmail)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}​
+
+	$('#submit').click(function() {
+		var sEmail = $('#email').val();
+		if ($.trim(sEmail).length == 0) {
+			alert('Please enter valid email address');
+			e.preventDefault();
+		}
+		if (validateEmail(sEmail)) {
+			alert('Email is valid');
+		}
+		else {
+			alert('Invalid Email Address');
+			e.preventDefault();
+		}
+	});
+
+
+
+
+
+
+
+
+
+
+
 });
